8fb169917f6cd88e00e48097032963cf843f9c3b
tejas122125 <tejasweekumarsingh@gmail.com>
2024-02-14 20:05:31 +0530

    solved

--- server/socketio/connection.js
+++ server/socketio/connection.js
@@ -3,9 +3,7 @@ import { createServer } from 'http';
 import express from 'express';
 import { Server } from 'socket.io';
 import cors from 'cors'
-// import io from "socket.io"
 
-// Create an Express app
 const app = express();
 const server = createServer(app)
 const io = new Server(server,{
@@ -15,26 +13,11 @@ const io = new Server(server,{
     credentials:true,
   }
 })
-// app.use(cors({ origin:'http://localhost:5173', 
-// credentials:true,            
-// optionSuccessStatus:200}))
+
 app.use(cors())
 app.get("/test",(req,res)=>{
   res.send(JSON.stringify("hello monu bhai"))
 })
-// Create a Socket.IO server instance
-// const io = require("socket.io")(3000,{
-//   cors:{
-//     origin : ['http://localhost:5173'],
-//   }
-// })
-// io.origins('*:*');
-// io.use(cors(
-//  { origin:'http://localhost:5173', 
-//   credentials:true,            
-//   optionSuccessStatus:200}
-// ));
-
 io.on("connection",(socket)=>{
 console.log("monu is connecting")
 
