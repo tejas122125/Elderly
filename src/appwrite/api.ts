144de5356907f59c3def802b914d5210fff17d5c
tejas122125 <tejasweekumarsingh@gmail.com>
2024-02-18 23:41:06 +0530

    aaa

--- src/appwrite/api.ts
+++ src/appwrite/api.ts
@@ -18,7 +18,35 @@ try {
 
 }
 
+export async function createUserAccount(user) {
+    try {
+        const newAccount = await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name,
+        )
 
+        const avatarsUrl = avatars.getInitials(user.name)
+        const newUser = await saveuserToDb({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarsUrl,
+
+
+        })
+
+        return newUser;
+
+
+    }
+    catch (error) {
+        console.log("some error occurred in api.ts file regarding creating a new user in appwrite")
+    }
+
+}
 
 export const saveuserToDb = async (
     user: {
@@ -31,7 +59,7 @@ export const saveuserToDb = async (
 ) => {
 
     try {
-        const newUser = await databases.createDocument(appwriteConfig.databaseId,"658da7c34f70d1d51ef6", ID.unique(),user)
+        const newUser = await databases.createDocument(appwritedatabaseId,"658da7c34f70d1d51ef6", ID.unique(),user)
         return newUser
     }
     catch (error) {
