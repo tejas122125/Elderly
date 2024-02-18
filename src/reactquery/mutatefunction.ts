253d8cea9a30fcecf6927440a4b55e0a1bff588a
tejas122125 <tejasweekumarsingh@gmail.com>
2024-02-18 23:30:18 +0530

    aaa

--- src/reactquery/mutatefunction.ts
+++ src/reactquery/mutatefunction.ts
@@ -1,8 +1,6 @@
 import { uploadImage } from '@/appwrite/api';
 import { useMutation } from 'react-query';
 
-const [mutate, { isLoading, isError, isSuccess }] = useMutation(uploadimage);
-
 export const uploadImageMutation = () => {
     return useMutation({
         mutationFn: (file: File) => {
@@ -12,3 +10,6 @@ export const uploadImageMutation = () => {
 
     )
 }
+
+
+// uploading elders detatils
